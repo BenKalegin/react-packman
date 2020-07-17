e741736104c0b72422432b76353b80d8e91c0d44
Ben Kalegin <BenKalegin@users.noreply.github.com>
2020-07-16 21:39:55 -0400

    Updates

---
+++ precache-manifest.2ede2d1c97d6649b641184676203dea1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8369a083baa9bed4ef1",
+    "url": "/react-packman/static/css/main.0d0ca9b2.chunk.css"
+  },
+  {
+    "revision": "f8369a083baa9bed4ef1",
+    "url": "/react-packman/static/js/main.f8369a08.chunk.js"
+  },
+  {
+    "revision": "05f91bbe43b5bd63ed69",
+    "url": "/react-packman/static/js/1.05f91bbe.chunk.js"
+  },
+  {
+    "revision": "eeda74cc7405a8044fc5",
+    "url": "/react-packman/static/js/runtime~main.eeda74cc.js"
+  },
+  {
+    "revision": "a074351b01c6695875d7651ff3f05ad1",
+    "url": "/react-packman/index.html"
+  }
+];
